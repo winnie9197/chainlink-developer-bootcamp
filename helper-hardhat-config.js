6fc2e81f0d6b3ac4a71050507b9c330072b9db28
winniehcy <winniehcyeung@gmail.com>
2021-07-25 13:58:14 -0400

    local Hardhat network with forked Ethereum mainnet

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,17 +1,18 @@
 const networkConfig = {
-    default: {
+    default: { 
         name: 'hardhat',
         fee: '100000000000000000',
         keyHash: '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
         jobId: '29fa9aa13bf1468788b7cc4a500a45b8',
         fundAmount: "1000000000000000000"
     },
-    31337: {
+    31337: { //31337 Network: local, forked Hardhat network
         name: 'localhost',
         fee: '100000000000000000',
         keyHash: '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4',
         jobId: '29fa9aa13bf1468788b7cc4a500a45b8',
-        fundAmount: "1000000000000000000"
+        fundAmount: "1000000000000000000",
+        ethUsdPriceFeed: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419' //ETH/USD price feed  contract on mainnet
     },
     42: {
         name: 'kovan',
